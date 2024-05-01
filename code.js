fdfae002620bf10f659bc1b42773ec80d21f0665
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 23:43:16 -0600

    code.js

--- code.js
+++ code.js
@@ -1,21 +1,3 @@
 function areIsomorphic(graph1, graph2) {
-    if (Object.keys(graph1).length !== Object.keys(graph2).length) return false;
-
-    const signature1 = graphSignature(graph1);
-    const signature2 = graphSignature(graph2);
-
-    return signature1 === signature2;
-}
-
-function graphSignature(graph) {
-    const signature = [];
-    const vertices = Object.keys(graph).sort();
-    for (const vertex of vertices) {
-        const edges = graph[vertex];
-        const sortedEdges = [...edges].sort();
-        signature.push(vertex);
-        signature.push(edges.length);
-        signature.push(...sortedEdges);
-    }
-    return signature.join('');
+    return;
 }
