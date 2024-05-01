285c8372794f1735ff7cd9cbfa99d1fb59aa1888
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 23:17:29 -0600

    code.js

--- code.js
+++ code.js
@@ -1,26 +1,10 @@
 function are_isomorphic(graph1, graph2) {
-    if (Object.keys(graph1).length !== Object.keys(graph2).length) {
+    let vertices1 = Object.keys(graph1);
+    let vertices2 = Object.keys(graph2);
+    if (vertices1.length !== vertices2.length) {
         return false;
     }
-    const degrees1 = Object.values(graph1).map(neighbors => neighbors.length);
-    const degrees2 = Object.values(graph2).map(neighbors => neighbors.length);
-    degrees1.sort((a, b) => a - b);
-    degrees2.sort((a, b) => a - b);
-    if (JSON.stringify(degrees1) !== JSON.stringify(degrees2)) {
-        return false;
-    }
-    for (let v1 in graph1) {
-        let matched = false;
-        for (let v2 in graph2) {
-            if (JSON.stringify(graph1[v1]) === JSON.stringify(graph2[v2])) {
-                delete graph2[v2];
-                matched = true;
-                break;
-            }
-        }
-        if (!matched) {
-            return false;
-        }
-    }
-    return true;
+    let sortedNeighbors1 = vertices1.map(v => graph1[v].sort().join(',')).sort();
+    let sortedNeighbors2 = vertices2.map(v => graph2[v].sort().join(',')).sort();
+    return sortedNeighbors1.join('-') === sortedNeighbors2.join('-');
 }
